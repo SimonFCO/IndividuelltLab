a7379e32061105d82f5a8e631604e58015cd4913
simonFCO <simon.vare6@gmail.com>
2026-03-31 17:45:39 +0200

    small title fix

--- js/Title.js
+++ js/Title.js
@@ -1,10 +1,9 @@
 const titleElement = document.getElementById("Title");
 const defaultTitle = "SimonFCO Dashboard";
-
 const savedTitle = localStorage.getItem("dashboardTitle");
 
 if (savedTitle) {
-  titleElement.innerText = savedTitle;
+  titleElement.innerHTML = `<h1>${savedTitle}</h1>`;
 }
 
 titleElement.addEventListener("input", function () {
